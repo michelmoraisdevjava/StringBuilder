12da2e36d8b0b01ae66c419a675d6a9899d68107
Michel Morais <valentimmichel@gmail.com>
2022-06-17 20:46:34 -0300

    Projeto StringBuilder

---
+++ src/entities/Post.java
@@ -0,0 +1,16 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+	
+	private Date moment;
+	private String title;
+	private String content;
+	private Integer likes;
+	
+	private List<Comment> comments = new ArrayList<>();
+
+}
