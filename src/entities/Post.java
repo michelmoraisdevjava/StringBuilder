8858975a1c65f48ea81889cf0eebf34875e513fe
Michel Morais <valentimmichel@gmail.com>
2022-06-17 21:06:22 -0300

    Criação dos construtores e Gets and sets Classe Post

--- src/entities/Post.java
+++ src/entities/Post.java
@@ -13,6 +13,50 @@ public class Post {
 	
 	private List<Comment> comments = new ArrayList<>();
 	
-	
+	public Post() {
+	}
+
+	public Post(Date moment, String title, String content, Integer likes) {
+		this.moment = moment;
+		this.title = title;
+		this.content = content;
+		this.likes = likes;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
 
 }
