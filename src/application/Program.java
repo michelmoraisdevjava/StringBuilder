b97fa7672e0b619c53888df595bbab601f965e87
Michel Morais <valentimmichel@gmail.com>
2022-06-17 21:51:05 -0300

    Adição comentários c1 e c2 associando ao Post p1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,23 @@
 package application;
 
-import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import entities.Comment;
+import entities.Post;
 
 public class Program {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws ParseException {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
-		sc.close();
+		Comment c1 = new Comment("Have a  nice trip!");
+		Comment c2 = new Comment("Wow that's awesome!");
+		Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"), 
+				"Traveling to new Zealand", 
+				"I'm going to visit this wonderful country!", 
+				12);
 
 	}
 
